1ef0bc87015e76673025a78a41beb97eb6dda0ea
ASteinheiser <me@iamandrew.io>
2025-03-17 01:08:30 -0700

    add fish config and update readme

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This is my personal starship config. Its basically a custom color themed version
 Read more about [starship here](https://starship.rs/)!
 
 <img src="https://github.com/ASteinheiser/starship-config/blob/main/fancy-prompt.png?raw=true" height="auto" width="800px" >
+
+_I also included my fish config, which automatically runs starship and tries to use_ `nvm` _to switch node versions when changing directories_
---
+++ config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function __check_nvm --on-variable PWD --description 'Do nvm stuff'
+  if test -f .nvmrc
+    set node_version (node -v)
+    set node_version_target (cat .nvmrc)
+    set nvmrc_node_version (nvm list | grep $node_version_target)
+
+    if string match -q -- "*$node_version" $nvmrc_node_version
+      # already current node version
+    else if not set -q $nvmrc_node_version
+      # install
+      nvm install $node_version_target
+    else
+      nvm use $node_version_target
+    end
+  end
+end
+
+__check_nvm
+
+starship init fish | source
